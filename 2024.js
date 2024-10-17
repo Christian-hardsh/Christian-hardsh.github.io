de5c12678456f094977c3398bc2cd5c427c72f68
Christian-hardsh <e2045144@edu.mt.gov.br>
2024-10-17 09:58:40 -0400

    ..

--- 2024.js
+++
@@ -1,9 +0,0 @@
-let currentSlide = 0;
-
-function moveSlide(step) {
-    const slides = document.querySelectorAll('.carousel-slide');
-    const totalSlides = slides.length;
-    currentSlide = (currentSlide + step + totalSlides) % totalSlides;
-    const offset = -currentSlide * 100;
-    document.querySelector('.carousel-wrapper').style.transform = `translateX(${offset}%)`;
-}
